b3227557a7d7a65233a4ed38c722950d6b6be885
omorales <octaviomr9810@gmail.com>
2024-11-29 16:25:24 -0600

    fix: quitar seccion adicional

--- src/app/page.js
+++ src/app/page.js
@@ -15,34 +15,6 @@ export default function Home() {
 
         {/* Accesos directos*/}
         <Shortcuts></Shortcuts>
-
-        {/* Transacciones */}
-        <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Últimas Transacciones
-          </h2>
-          <ul className="space-y-4">
-            <li className="bg-white p-4 rounded-lg shadow-md">
-              <p className="text-gray-800 font-semibold">
-                Transferencia a Juan Pérez
-              </p>
-              <p className="text-gray-600">Monto: $500.00</p>
-              <p className="text-gray-600">Fecha: 10 de Noviembre, 2024</p>
-            </li>
-            <li className="bg-white p-4 rounded-lg shadow-md">
-              <p className="text-gray-800 font-semibold">Depósito de nómina</p>
-              <p className="text-gray-600">Monto: $1,200.00</p>
-              <p className="text-gray-600">Fecha: 9 de Noviembre, 2024</p>
-            </li>
-            <li className="bg-white p-4 rounded-lg shadow-md">
-              <p className="text-gray-800 font-semibold">
-                Pago de tarjeta de crédito
-              </p>
-              <p className="text-gray-600">Monto: $150.00</p>
-              <p className="text-gray-600">Fecha: 8 de Noviembre, 2024</p>
-            </li>
-          </ul>
-        </section>
       </main>
     </div>
   );
